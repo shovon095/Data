b2c5100530ccb80acdf4013722183579ccb80eb5
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-04-29 20:58:48 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,11 +1,12 @@
-/home/shouvon/miniconda3/lib/python3.11/site-packages/lightning/pytorch/trainer/connectors/data_connector.py:425: The 'train_dataloader' does not have many workers which may be a bottleneck. Consider increasing the value of the `num_workers` argument` to `num_workers=39` in the `DataLoader` to improve performance.
-/home/shouvon/miniconda3/lib/python3.11/site-packages/lightning/pytorch/trainer/connectors/data_connector.py:425: The 'val_dataloader' does not have many workers which may be a bottleneck. Consider increasing the value of the `num_workers` argument` to `num_workers=39` in the `DataLoader` to improve performance.
-Epoch 1/50:   0%|                                                                                                                                        | 0/50 [00:00<?, ?it/s]/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/module/_vae.py:569: UserWarning: The value argument must be within the support of the distribution
-  reconst_loss = -generative_outputs[MODULE_KEYS.PX_KEY].log_prob(x).sum(-1)
-/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/module/_vae.py:569: UserWarning: The value argument must be within the support of the distribution
-  reconst_loss = -generative_outputs[MODULE_KEYS.PX_KEY].log_prob(x).sum(-1)
-
-Epoch 2/50:   2%|█▍                                                                   | 1/50 [00:14<11:28, 14.06s/it, v_num=1, train_loss_step=7.6e+3, train_loss_epoch=7.34e+3]/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/module/_vae.py:569: UserWarning: The value argument must be within the support of the distribution
-  reconst_loss = -generative_outputs[MODULE_KEYS.PX_KEY].log_prob(x).sum(-1)
-/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/module/_vae.py:569: UserWarning: The value argument must be within the support of the distribution
-  reconst_loss = -generative_outputs[MODULE_KEYS.PX_KEY].log_prob(x).sum(-1)
+Traceback (most recent call last):
+  File "/home/shouvon/bioinform/./analyze_clusters.py", line 23, in <module>
+    args=p.parse_args(); main(args)
+                         ^^^^^^^^^^
+  File "/home/shouvon/bioinform/./analyze_clusters.py", line 10, in main
+    sc.tl.umap(ad); sc.tl.leiden(ad, resolution=0.5)
+                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scanpy/tools/_leiden.py", line 128, in leiden
+    _utils.ensure_igraph()
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scanpy/_utils/__init__.py", line 109, in ensure_igraph
+    raise ImportError(msg)
+ImportError: Please install the igraph package: `conda install -c conda-forge python-igraph` or `pip3 install igraph`.
