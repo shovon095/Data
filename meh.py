99827506f70701043891a0a5a2f813137f76e0ec
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-04 13:25:23 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,22 +1,5 @@
-Traceback (most recent call last):
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/pandas/core/indexes/base.py", line 3805, in get_loc
-    return self._engine.get_loc(casted_key)
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "index.pyx", line 167, in pandas._libs.index.IndexEngine.get_loc
-  File "index.pyx", line 196, in pandas._libs.index.IndexEngine.get_loc
-  File "pandas/_libs/hashtable_class_helper.pxi", line 7081, in pandas._libs.hashtable.PyObjectHashTable.get_item
-  File "pandas/_libs/hashtable_class_helper.pxi", line 7089, in pandas._libs.hashtable.PyObjectHashTable.get_item
-KeyError: 'cluster'
 
-The above exception was the direct cause of the following exception:
+What is the unabbreviated mailing address of the school with the highest FRPM count for K-12 students?
 
-Traceback (most recent call last):
-  File "/home/shouvon/bioinform/bar.py", line 12, in <module>
-    ct = (pd.crosstab(ad.obs["cluster"], ad.obs["genotype"])
-                      ~~~~~~^^^^^^^^^^^
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/pandas/core/frame.py", line 4102, in __getitem__
-    indexer = self.columns.get_loc(key)
-              ^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/pandas/core/indexes/base.py", line 3812, in get_loc
-    raise KeyError(key) from err
-KeyError: 'cluster'
+Baseline- GPT-4o: "SELECT schools.MailStreet, schools.MailCity, schools.MailState, schools.MailZip\nFROM frpm\nJOIN schools ON frpm.CDSCode = schools.CDSCode\nORDER BY `FRPM Count (K-12)` DESC\nLIMIT 1\t----- bird -----\tcalifornia_schools",
+attention -GpT-4o+Attention": "SELECT frpm.`Percent (%) Eligible Free (Ages 5-17)`\nFROM frpm\nJOIN schools ON frpm.CDSCode = schools.CDSCode\nWHERE schools.`EdOpsName` = 'Continuation'\nORDER BY frpm.`Percent (%) Eligible Free (Ages 5-17)` ASC\nLIMIT 3\t----- bird -----\tcalifornia_schools"
