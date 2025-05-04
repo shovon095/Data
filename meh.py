1474593c3a1bf104f387eda6aba90d05529ce364
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-04 13:29:35 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,5 +1,1 @@
-
-What is the unabbreviated mailing address of the school with the highest FRPM count for K-12 students?
-
-Baseline- GPT-4o: "SELECT schools.MailStreet, schools.MailCity, schools.MailState, schools.MailZip\nFROM frpm\nJOIN schools ON frpm.CDSCode = schools.CDSCode\nORDER BY `FRPM Count (K-12)` DESC\nLIMIT 1\t----- bird -----\tcalifornia_schools",
-attention -GpT-4o+Attention": "SELECT frpm.`Percent (%) Eligible Free (Ages 5-17)`\nFROM frpm\nJOIN schools ON frpm.CDSCode = schools.CDSCode\nWHERE schools.`EdOpsName` = 'Continuation'\nORDER BY frpm.`Percent (%) Eligible Free (Ages 5-17)` ASC\nLIMIT 3\t----- bird -----\tcalifornia_schools"
+    "3": "SELECT schools.MailStreet, schools.MailCity, schools.MailZip, schools.MailState\nFROM frpm\nJOIN schools ON frpm.CDSCode = schools.CDSCode\nORDER BY frpm.`FRPM Count (K-12)` DESC\nLIMIT 1\t----- bird -----\tcalifornia_schools",
