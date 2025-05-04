f46e49f6c01a14867ce5f2c6593ce0e658e7f8c0
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-04 13:39:11 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,3 +1,35 @@
+        "question_id": 0,
+        "db_id": "california_schools",
+        "question": "What is the highest eligible free rate for K-12 students in the schools in Alameda County?",
+        "evidence": "Eligible free rate for K-12 = `Free Meal Count (K-12)` / `Enrollment (K-12)`",
+        "SQL": "SELECT `Free Meal Count (K-12)` / `Enrollment (K-12)` FROM frpm WHERE `County Name` = 'Alameda' ORDER BY (CAST(`Free Meal Count (K-12)` AS REAL) / `Enrollment (K-12)`) DESC LIMIT 1",
+        "difficulty": "simple"
+    },
+
+
+    {
+        "question_id": 90,
+        "db_id": "financial",
+        "question": "How many accounts who have region in Prague are eligible for loans?",
+        "evidence": "A3 contains the data of region",
+        "SQL": "SELECT COUNT(T1.account_id) FROM account AS T1 INNER JOIN loan AS T2 ON T1.account_id = T2.account_id INNER JOIN district AS T3 ON T1.district_id = T3.district_id WHERE T3.A3 = 'Prague'",
+        "difficulty": "simple"
+    },
+
+ {                                                                                                                                                               "question_id": 118,                                                                                                                                         "db_id": "financial",                                                                                                                                       "question": "For loan amount less than USD100,000, what is the percentage of accounts that is still running with no issue.",                                "evidence": "Status = 'C' stands for running contract, ok so far; Percentage of accounts by condition = [(total(amount) & condition) / (total amount)] * 100%.",
+        "SQL": "SELECT CAST(SUM(status = 'C') AS REAL) * 100 / COUNT(amount) FROM loan WHERE amount < 100000",
+        "difficulty": "moderate"
+    },
+
+    {
+        "question_id": 1,
+        "db_id": "california_schools",
+        "question": "Please list the lowest three eligible free rates for students aged 5-17 in continuation schools.",
+        "evidence": "Eligible free rates for students aged 5-17 = `Free Meal Count (Ages 5-17)` / `Enrollment (Ages 5-17)`",
+        "SQL": "SELECT `Free Meal Count (Ages 5-17)` / `Enrollment (Ages 5-17)` FROM frpm WHERE `Educational Option Type` = 'Continuation School' AND `Free Meal Count (Ages 5-17)` / `Enrollment (Ages 5-17)` IS NOT NULL ORDER BY `Free Meal Count (Ages 5-17)` / `Enrollment (Ages 5-17)` ASC LIMIT 3",
+        "difficulty": "moderate"
+    }
+
 Baseline:
 {
     "0": "SELECT MAX(`Percent (%) Eligible Free (K-12)`) AS MaxEligibleFreeRate\nFROM frpm\nJOIN schools ON frpm.CDSCode = schools.CDSCode\nWHERE schools.County = 'Alameda'\t----- bird -----\tcalifornia_schools",
