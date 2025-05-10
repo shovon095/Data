4a40b0e4049eeb47a596ac185dec0485825ae299
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-09 21:29:37 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,15 +1,25 @@
-(base) shouvon@dxs4-DGX-Station:~/bioinform$ python train.py   --input     ./tms_concat.h5ad   --output    ./scvi_tms   --annotated ./TMS_scVI_integrated.h5ad
+ python no_adap.py \
+>   --input     5xFAD_matched_with_protocol.h5ad \
+model_d>   --model_dir scvi_tms \
+>   --output    results/fad_no_adapter.h5ad
 /home/shouvon/miniconda3/lib/python3.11/site-packages/docrep/decorators.py:43: SyntaxWarning: 'param_categorical_covariate_keys' is not a valid key!
   doc = func(self, args[0].__doc__, *args[1:], **kwargs)
 /home/shouvon/miniconda3/lib/python3.11/site-packages/docrep/decorators.py:43: SyntaxWarning: 'param_continuous_covariate_keys' is not a valid key!
   doc = func(self, args[0].__doc__, *args[1:], **kwargs)
-GPU available: True (cuda), used: True
-TPU available: False, using: 0 TPU cores
-HPU available: False, using: 0 HPUs
-LOCAL_RANK: 0 - CUDA_VISIBLE_DEVICES: [0,1,2,3]
-/home/shouvon/miniconda3/lib/python3.11/site-packages/lightning/pytorch/trainer/connectors/data_connector.py:425: The 'train_dataloader' does not have many workers which may be a bottleneck. Consider increasing the value of the `num_workers` argument` to `num_workers=39` in the `DataLoader` to improve performance.
-/home/shouvon/miniconda3/lib/python3.11/site-packages/lightning/pytorch/trainer/connectors/data_connector.py:425: The 'val_dataloader' does not have many workers which may be a bottleneck. Consider increasing the value of the `num_workers` argument` to `num_workers=39` in the `DataLoader` to improve performance.
-Epoch 20/20: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████| 20/20 [08:35<00:00, 25.77s/it, v_num=1, train_loss_step=9.58e+3, train_loss_epoch=1.03e+4]`Trainer.fit` stopped: `max_epochs=20` reached.
-Epoch 20/20: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████| 20/20 [08:35<00:00, 25.77s/it, v_num=1, train_loss_step=9.58e+3, train_loss_epoch=1.03e+4]
-
-[train_scvi] Model saved to ./scvi_tms and annotated data to ./TMS_scVI_integrated.h5ad
+INFO     File scvi_tms/model.pt already downloaded
+/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/data/fields/_base_field.py:63: UserWarning: adata.X does not contain unnormalized count data. Are you sure this is what you want?
+  self.validate_field(adata)
+Traceback (most recent call last):
+  File "/home/shouvon/bioinform/no_adap.py", line 47, in <module>
+    main(p.parse_args())
+  File "/home/shouvon/bioinform/no_adap.py", line 23, in main
+    ad.obsm["X_scVI_noAdapter"] = model.get_latent_representation()
+                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/torch/utils/_contextlib.py", line 116, in decorate_context
+    return func(*args, **kwargs)
+           ^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/model/base/_vaemixin.py", line 279, in get_latent_representation
+    self._check_if_trained(warn=False)
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/model/base/_base_model.py", line 490, in _check_if_trained
+    raise RuntimeError(message)
+RuntimeError: Trying to query inferred values from an untrained model. Please train the model first.
