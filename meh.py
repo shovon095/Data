7be3bfb6fb8b6d31d065a90e5165ba174c9b8b6b
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-09 21:39:00 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,25 +1,29 @@
- python no_adap.py \
->   --input     5xFAD_matched_with_protocol.h5ad \
-model_d>   --model_dir scvi_tms \
->   --output    results/fad_no_adapter.h5ad
-/home/shouvon/miniconda3/lib/python3.11/site-packages/docrep/decorators.py:43: SyntaxWarning: 'param_categorical_covariate_keys' is not a valid key!
-  doc = func(self, args[0].__doc__, *args[1:], **kwargs)
-/home/shouvon/miniconda3/lib/python3.11/site-packages/docrep/decorators.py:43: SyntaxWarning: 'param_continuous_covariate_keys' is not a valid key!
-  doc = func(self, args[0].__doc__, *args[1:], **kwargs)
-INFO     File scvi_tms/model.pt already downloaded
+ File scvi_tms/model.pt already downloaded
 /home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/data/fields/_base_field.py:63: UserWarning: adata.X does not contain unnormalized count data. Are you sure this is what you want?
   self.validate_field(adata)
+
+/home/shouvon/bioinform/no_adap.py:25: FutureWarning: In the future, the default backend for leiden will be igraph instead of leidenalg.
+
+ To achieve the future defaults please pass: flavor="igraph" and n_iterations=2.  directed must also be False to work with igraph's implementation.
+  sc.tl.leiden(ad, resolution=0.5)   # optional colouring
+
 Traceback (most recent call last):
-  File "/home/shouvon/bioinform/no_adap.py", line 47, in <module>
+  File "/home/shouvon/bioinform/no_adap.py", line 44, in <module>
     main(p.parse_args())
-  File "/home/shouvon/bioinform/no_adap.py", line 23, in main
-    ad.obsm["X_scVI_noAdapter"] = model.get_latent_representation()
-                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/torch/utils/_contextlib.py", line 116, in decorate_context
-    return func(*args, **kwargs)
-           ^^^^^^^^^^^^^^^^^^^^^
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/model/base/_vaemixin.py", line 279, in get_latent_representation
-    self._check_if_trained(warn=False)
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/model/base/_base_model.py", line 490, in _check_if_trained
-    raise RuntimeError(message)
-RuntimeError: Trying to query inferred values from an untrained model. Please train the model first.
+  File "/home/shouvon/bioinform/no_adap.py", line 28, in main
+    sc.pl.umap(
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scanpy/plotting/_tools/scatterplots.py", line 686, in umap
+    return embedding(adata, "umap", **kwargs)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scanpy/plotting/_tools/scatterplots.py", line 279, in embedding
+    color_source_vector = _get_color_source_vector(
+                          ^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scanpy/plotting/_tools/scatterplots.py", line 1200, in _get_color_source_vector
+    values = adata.obs_vector(value_to_plot, layer=layer)
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/anndata/_core/anndata.py", line 1297, in obs_vector
+    return get_vector(self, k, "obs", "var", layer=layer)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/anndata/_core/index.py", line 245, in get_vector
+    raise KeyError(msg)
+KeyError: 'Could not find key dataset in .var_names or .obs.columns.'
