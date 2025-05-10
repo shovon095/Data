c0c651684c21fd1f88e93189511c4ee4556bd5c6
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-09 21:07:03 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,20 +1,21 @@
+home/shouvon/miniconda3/lib/python3.11/site-packages/docrep/decorators.py:43: SyntaxWarning: 'param_categorical_covariate_keys' is not a valid key!
+  doc = func(self, args[0].__doc__, *args[1:], **kwargs)
+/home/shouvon/miniconda3/lib/python3.11/site-packages/docrep/decorators.py:43: SyntaxWarning: 'param_continuous_covariate_keys' is not a valid key!
+  doc = func(self, args[0].__doc__, *args[1:], **kwargs)
+INFO     File scvi_tms/model.pt already downloaded
 /home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/data/fields/_base_field.py:63: UserWarning: adata.X does not contain unnormalized count data. Are you sure this is what you want?
   self.validate_field(adata)
 Traceback (most recent call last):
-  File "/home/shouvon/bioinform/no_adap.py", line 54, in <module>
-    main(args)
-  File "/home/shouvon/bioinform/no_adap.py", line 21, in main
-    model = scvi.model.SCVI.load(args.model_dir, adata=ad)
-            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/model/base/_base_model.py", line 731, in load
-    getattr(cls, method_name)(adata, source_registry=registry, **registry[_SETUP_ARGS_KEY])
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/model/_scvi.py", line 223, in setup_anndata
-    adata_manager.register_fields(adata, **kwargs)
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/data/_manager.py", line 185, in register_fields
-    self._add_field(
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/data/_manager.py", line 220, in _add_field
-    field_registry[_constants._STATE_REGISTRY_KEY] = field.transfer_field(
-                                                     ^^^^^^^^^^^^^^^^^^^^^
-  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/data/fields/_dataframe_field.py", line 221, in transfer_field
-    raise ValueError(
-ValueError: Category query not found in source registry. Cannot transfer setup without `extend_categories = True`.
+  File "/home/shouvon/bioinform/no_adap.py", line 47, in <module>
+    main(p.parse_args())
+  File "/home/shouvon/bioinform/no_adap.py", line 23, in main
+    ad.obsm["X_scVI_noAdapter"] = model.get_latent_representation()
+                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/torch/utils/_contextlib.py", line 116, in decorate_context
+    return func(*args, **kwargs)
+           ^^^^^^^^^^^^^^^^^^^^^
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/model/base/_vaemixin.py", line 279, in get_latent_representation
+    self._check_if_trained(warn=False)
+  File "/home/shouvon/miniconda3/lib/python3.11/site-packages/scvi/model/base/_base_model.py", line 490, in _check_if_trained
+    raise RuntimeError(message)
+RuntimeError: Trying to query inferred values from an untrained model. Please train the model first.
