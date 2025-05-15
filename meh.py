f3e559f50e93456b7f311cbef0e29b87fcdaa9cd
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-14 23:26:12 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,15 +1,19 @@
-
-DKMS: install completed.
-Setting up xserver-xorg-video-nvidia-530:amd64 (535.146.02-0ubuntu0~gpu18.04.1) ...
-Setting up libnvidia-common-530 (535.146.02-0ubuntu0~gpu18.04.1) ...
-Setting up libnvidia-gl-530:amd64 (535.146.02-0ubuntu0~gpu18.04.1) ...
-Setting up nvidia-dkms-530 (535.146.02-0ubuntu0~gpu18.04.1) ...
-Setting up nvidia-driver-535 (535.146.02-0ubuntu0~gpu18.04.1) ...
-Setting up nvidia-driver-530 (535.146.02-0ubuntu0~gpu18.04.1) ...
-Processing triggers for man-db (2.8.3-2ubuntu0.1) ...
-Processing triggers for libc-bin (2.27-3ubuntu1.6) ...
-Processing triggers for initramfs-tools (0.130ubuntu3.13) ...
-update-initramfs: Generating /boot/initrd.img-4.15.0-213-generic
-shouvon@dgx1-DGX-Station:~$
-
-
+ip addr show
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host
+       valid_lft forever preferred_lft forever
+2: enp2s0f0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc mq state DOWN group default qlen 1000
+    link/ether 0c:9d:92:20:61:af brd ff:ff:ff:ff:ff:ff
+3: enp2s0f1: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc mq state UP group default qlen 1000
+    link/ether 0c:9d:92:20:61:b0 brd ff:ff:ff:ff:ff:ff
+    inet 10.32.25.147/23 brd 10.32.25.255 scope global dynamic noprefixroute enp2s0f1
+       valid_lft 28570sec preferred_lft 28570sec
+    inet6 fe80::d510:c591:9c48:301c/64 scope link noprefixroute
+       valid_lft forever preferred_lft forever
+4: docker0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc noqueue state DOWN group default
+    link/ether 02:42:c0:db:54:fa brd ff:ff:ff:ff:ff:ff
+    inet 172.17.0.1/16 brd 172.17.255.255 scope global docker0
+       valid_lft forever preferred_lft forever
