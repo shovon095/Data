5f3708ec5123bc4f89d8b084080b9638dd3263c6
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-14 23:16:54 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,4 +1,4 @@
- sudo apt-get install -y nvidia-driver-515 nvidia-dkms-515
+sudo apt install -y nvidia-driver-515
 Reading package lists... Done
 Building dependency tree
 Reading state information... Done
@@ -7,40 +7,5 @@ requested an impossible situation or if you are using the unstable
 distribution that some required packages have not yet been created
 or been moved out of Incoming.
 The following information may help to resolve the situation:
-
-The following packages have unmet dependencies:
- nvidia-driver-515 : Depends: libnvidia-gl-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-compute-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-extra-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: nvidia-compute-utils-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-decode-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-encode-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: nvidia-utils-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: xserver-xorg-video-nvidia-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-cfg1-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-fbc1-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-E: Unable to correct problems, you have held broken packages.
-shouvon@dgx1-DGX-Station:~$ sudo apt install -y nvidia-driver-515
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-Some packages could not be installed. This may mean that you have
-requested an impossible situation or if you are using the unstable
-distribution that some required packages have not yet been created
-or been moved out of Incoming.
-The following information may help to resolve the situation:
-
-The following packages have unmet dependencies:
- nvidia-driver-515 : Depends: libnvidia-gl-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-compute-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-extra-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: nvidia-compute-utils-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-decode-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-encode-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: nvidia-utils-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: xserver-xorg-video-nvidia-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-cfg1-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-                     Depends: libnvidia-fbc1-515 (= 515.105.01-0ubuntu0.18.04.1) but it is not going to be installed
-E: Unable to correct problems, you have held broken packages.
 
 
