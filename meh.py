77507a7b5c90c60588fc90f7c84702d7a549744b
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-14 23:48:19 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,6 +1,4 @@
- nvcc --version
-nvcc: NVIDIA (R) Cuda compiler driver
-Copyright (c) 2005-2022 NVIDIA Corporation
-Built on Wed_Jun__8_16:49:14_PDT_2022
-Cuda compilation tools, release 11.7, V11.7.99
-Build cuda_11.7.r11.7/compiler.31442593_0
+Torch version:      1.10.1+cu102
+CUDA built with:    10.2
+CUDA available?:    True
+GPU device name:    Tesla V100-DGXS-32GB
