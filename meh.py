75409b567778f80e5c719c62ad0aa9f800a98248
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-14 23:33:19 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,19 +1,33 @@
-ip addr show
-1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
-    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
-    inet 127.0.0.1/8 scope host lo
-       valid_lft forever preferred_lft forever
-    inet6 ::1/128 scope host
-       valid_lft forever preferred_lft forever
-2: enp2s0f0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc mq state DOWN group default qlen 1000
-    link/ether 0c:9d:92:20:61:af brd ff:ff:ff:ff:ff:ff
-3: enp2s0f1: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc mq state UP group default qlen 1000
-    link/ether 0c:9d:92:20:61:b0 brd ff:ff:ff:ff:ff:ff
-    inet 10.32.25.147/23 brd 10.32.25.255 scope global dynamic noprefixroute enp2s0f1
-       valid_lft 28570sec preferred_lft 28570sec
-    inet6 fe80::d510:c591:9c48:301c/64 scope link noprefixroute
-       valid_lft forever preferred_lft forever
-4: docker0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc noqueue state DOWN group default
-    link/ether 02:42:c0:db:54:fa brd ff:ff:ff:ff:ff:ff
-    inet 172.17.0.1/16 brd 172.17.255.255 scope global docker0
-       valid_lft forever preferred_lft forever
+nvidia-smi
+Wed May 14 23:31:29 2025
++---------------------------------------------------------------------------------------+
+| NVIDIA-SMI 535.146.02             Driver Version: 535.146.02   CUDA Version: 12.2     |
+|-----------------------------------------+----------------------+----------------------+
+| GPU  Name                 Persistence-M | Bus-Id        Disp.A | Volatile Uncorr. ECC |
+| Fan  Temp   Perf          Pwr:Usage/Cap |         Memory-Usage | GPU-Util  Compute M. |
+|                                         |                      |               MIG M. |
+|=========================================+======================+======================|
+|   0  Tesla V100-DGXS-32GB           On  | 00000000:07:00.0  On |                    0 |
+| N/A   35C    P0              36W / 300W |    128MiB / 32768MiB |      0%      Default |
+|                                         |                      |                  N/A |
++-----------------------------------------+----------------------+----------------------+
+|   1  Tesla V100-DGXS-32GB           On  | 00000000:08:00.0 Off |                    0 |
+| N/A   34C    P0              37W / 300W |      1MiB / 32768MiB |      0%      Default |
+|                                         |                      |                  N/A |
++-----------------------------------------+----------------------+----------------------+
+|   2  Tesla V100-DGXS-32GB           On  | 00000000:0E:00.0 Off |                    0 |
+| N/A   35C    P0              38W / 300W |      1MiB / 32768MiB |      0%      Default |
+|                                         |                      |                  N/A |
++-----------------------------------------+----------------------+----------------------+
+|   3  Tesla V100-DGXS-32GB           On  | 00000000:0F:00.0 Off |                    0 |
+| N/A   34C    P0              36W / 300W |      1MiB / 32768MiB |      0%      Default |
+|                                         |                      |                  N/A |
++-----------------------------------------+----------------------+----------------------+
+
++---------------------------------------------------------------------------------------+
+| Processes:                                                                            |
+|  GPU   GI   CI        PID   Type   Process name                            GPU Memory |
+|        ID   ID                                                             Usage      |
+|=======================================================================================|
+|    0   N/A  N/A      2127      G   /usr/lib/xorg/Xorg                           39MiB |
+|    0   N/A  N/A      2601      G   /usr/bin/gnome-shell                         86MiB |
