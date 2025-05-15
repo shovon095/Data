7b86dedfab28fd75bb36c5ff4944c1d6580eacd0
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-14 23:02:04 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,5 +1,8 @@
-W: An error occurred during the signature verification. The repository is not updated and the previous index files will be used. GPG error: https://international.download.nvidia.com/dgxstation/repos/bionic bionic InRelease: The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 208CE844D9F220AD
-W: Failed to fetch http://international.download.nvidia.com/dgxstation/repos/bionic/dists/bionic/InRelease  The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 208CE844D9F220AD
-W: Some index files failed to download. They have been ignored, or old ones used instead.
+sudo ubuntu-drivers list
+WARNING:root:_pkg_get_support nvidia-driver-515-server: package has invalid Support PBheader, cannot determine support level
+WARNING:root:_pkg_get_support nvidia-driver-390: package has invalid Support Legacyheader, cannot determine support level
+WARNING:root:_pkg_get_support nvidia-driver-525-server: package has invalid Support PBheader, cannot determine support level
+WARNING:root:_pkg_get_support nvidia-driver-535: package has invalid Support PBheader, cannot determine support level
+WARNING:root:_pkg_get_support nvidia-driver-545: package has invalid Support PBheader, cannot determine support level
 
 
