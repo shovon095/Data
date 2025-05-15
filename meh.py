64dcf1c08c94f8746c0be797fdf1c07b19d91554
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-14 22:24:31 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,3 +1,21 @@
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 208CE844D9F220AD
-Executing: /tmp/apt-key-gpghome.ducjdiBZxR/gpg.1.sh --keyserver keyserver.ubuntu.com --recv-keys 208CE844D9F220AD
-gpg: keyserver receive failed: Connection timed out
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+--2025-05-14 22:23:32--  https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+Resolving developer.download.nvidia.com (developer.download.nvidia.com)... 23.221.22.197, 23.221.22.207
+Connecting to developer.download.nvidia.com (developer.download.nvidia.com)|23.221.22.197|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 4332 (4.2K) [application/x-deb]
+Saving to: ‘cuda-keyring_1.0-1_all.deb’
+
+cuda-keyring_1.0-1_all.deb                  100%[===========================================================================================>]   4.23K  --.-KB/s    in 0s
+
+2025-05-14 22:23:33 (434 MB/s) - ‘cuda-keyring_1.0-1_all.deb’ saved [4332/4332]
+
+shouvon@dgx1-DGX-Station:~$ sudo dpkg -i cuda-keyring_1.0-1_all.deb
+Selecting previously unselected package cuda-keyring.
+(Reading database ... 236541 files and directories currently installed.)
+Preparing to unpack cuda-keyring_1.0-1_all.deb ...
+Unpacking cuda-keyring (1.0-1) ...
+Setting up cuda-keyring (1.0-1) ...
+shouvon@dgx1-DGX-Station:~$ sudo apt update
+E: Conflicting values set for option Signed-By regarding source https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /: /usr/share/keyrings/cuda-archive-keyring.gpg !=
+E: The list of sources could not be read.
