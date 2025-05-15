e838a78e98baf75aa96fb5b2bcc1c00554054ca1
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-15 00:02:09 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,8 +1,10 @@
-pip install torch==2.2.0+cu117 torchvision==0.17.2+cu117 torchaudio==2.2.2 --extra-index-url https://download.pytorch.org/whl/cu117
-Looking in indexes: https://pypi.org/simple, https://download.pytorch.org/whl/cu117
-ERROR: Could not find a version that satisfies the requirement torch==2.2.0+cu117 (from versions: 1.0.0, 1.0.1, 1.1.0, 1.2.0, 1.3.0, 1.3.1, 1.4.0, 1.5.0, 1.5.1, 1.6.0, 1.7.0, 1.7.1, 1.8.0, 1.8.1, 1.9.0, 1.9.1, 1.10.0, 1.10.1, 1.10.2)
-ERROR: No matching distribution found for torch==2.2.0+cu117
-(venv) shouvon@dgx1-DGX-Station:~/my_pt_env$ pip3 install torch==2.2.0+cu117 torchvision==0.17.2+cu117 torchaudio==2.2.2 --extra-index-url https://download.pytorch.org/whl/cu117
-Looking in indexes: https://pypi.org/simple, https://download.pytorch.org/whl/cu117
-ERROR: Could not find a version that satisfies the requirement torch==2.2.0+cu117 (from versions: 1.0.0, 1.0.1, 1.1.0, 1.2.0, 1.3.0, 1.3.1, 1.4.0, 1.5.0, 1.5.1, 1.6.0, 1.7.0, 1.7.1, 1.8.0, 1.8.1, 1.9.0, 1.9.1, 1.10.0, 1.10.1, 1.10.2)
-ERROR: No matching distribution found for torch==2.2.0+cu117
+Welcome to NVIDIA DGX Station Version 4.0.7 (GNU/Linux 4.15.0-213-generic x86_64)
+Last login: Wed May 14 23:39:16 2025 from 129.207.35.122
+shouvon@dgx1-DGX-Station:~$ which nvcc
+/usr/local/cuda/bin/nvcc
+shouvon@dgx1-DGX-Station:~$ ls -l /usr/local/cuda
+lrwxrwxrwx 1 root root 22 May 14 22:57 /usr/local/cuda -> /etc/alternatives/cuda
+shouvon@dgx1-DGX-Station:~$ sudo ln -sfn /usr/local/cuda-11.7 /usr/local/cuda
+[sudo] password for shouvon:
+shouvon@dgx1-DGX-Station:~$ source /etc/profile.d/cuda.sh
+-bash: /etc/profile.d/cuda.sh: No such file or directory
