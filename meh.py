c1c47e85d9d220a9779a36eb2f565327bd26fd56
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-14 22:29:22 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,21 +1,48 @@
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
---2025-05-14 22:23:32--  https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
-Resolving developer.download.nvidia.com (developer.download.nvidia.com)... 23.221.22.197, 23.221.22.207
-Connecting to developer.download.nvidia.com (developer.download.nvidia.com)|23.221.22.197|:443... connected.
-HTTP request sent, awaiting response... 200 OK
-Length: 4332 (4.2K) [application/x-deb]
-Saving to: ‘cuda-keyring_1.0-1_all.deb’
+apt-key list
+/etc/apt/trusted.gpg
+--------------------
+pub   rsa4096 2022-04-14 [SC]
+      EB69 3B30 35CD 5710 E231  E123 A4B4 6996 3BF8 63CC
+uid           [ unknown] cudatools <cudatools@nvidia.com>
 
-cuda-keyring_1.0-1_all.deb                  100%[===========================================================================================>]   4.23K  --.-KB/s    in 0s
+/etc/apt/trusted.gpg.d/dgx-key.gpg
+----------------------------------
+pub   rsa4096 2016-06-07 [SC]
+      7835 264B 12C3 DA96 5D0E  1078 ECDB 3674 629C 85F2
+uid           [ unknown] dgx-cosmos-support <dgx-cosmos-support@nvidia.com>
+sub   rsa4096 2016-06-07 [E]
 
-2025-05-14 22:23:33 (434 MB/s) - ‘cuda-keyring_1.0-1_all.deb’ saved [4332/4332]
+/etc/apt/trusted.gpg.d/docker-key.gpg
+-------------------------------------
+pub   rsa4096 2017-02-22 [SCEA]
+      9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
+uid           [ unknown] Docker Release (CE deb) <docker@docker.com>
+sub   rsa4096 2017-02-22 [S]
 
-shouvon@dgx1-DGX-Station:~$ sudo dpkg -i cuda-keyring_1.0-1_all.deb
-Selecting previously unselected package cuda-keyring.
-(Reading database ... 236541 files and directories currently installed.)
-Preparing to unpack cuda-keyring_1.0-1_all.deb ...
-Unpacking cuda-keyring (1.0-1) ...
-Setting up cuda-keyring (1.0-1) ...
-shouvon@dgx1-DGX-Station:~$ sudo apt update
-E: Conflicting values set for option Signed-By regarding source https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /: /usr/share/keyrings/cuda-archive-keyring.gpg !=
-E: The list of sources could not be read.
+/etc/apt/trusted.gpg.d/graphics-drivers_ubuntu_ppa.gpg
+------------------------------------------------------
+pub   rsa4096 2015-08-12 [SC]
+      2388 FF3B E10A 76F6 38F8  0723 FCAE 110B 1118 213C
+uid           [ unknown] Launchpad PPA for Graphics Drivers Team
+
+/etc/apt/trusted.gpg.d/ubuntu-keyring-2012-archive.gpg
+------------------------------------------------------
+pub   rsa4096 2012-05-11 [SC]
+      790B C727 7767 219C 42C8  6F93 3B4F E6AC C0B2 1F32
+uid           [ unknown] Ubuntu Archive Automatic Signing Key (2012) <ftpmaster@ubuntu.com>
+
+/etc/apt/trusted.gpg.d/ubuntu-keyring-2012-cdimage.gpg
+------------------------------------------------------
+pub   rsa4096 2012-05-11 [SC]
+      8439 38DF 228D 22F7 B374  2BC0 D94A A3F0 EFE2 1092
+uid           [ unknown] Ubuntu CD Image Automatic Signing Key (2012) <cdimage@ubuntu.com>
+
+/etc/apt/trusted.gpg.d/ubuntu-keyring-2018-archive.gpg
+------------------------------------------------------
+pub   rsa4096 2018-09-17 [SC]
+      F6EC B376 2474 EDA9 D21B  7022 8719 20D1 991B C93C
+uid           [ unknown] Ubuntu Archive Automatic Signing Key (2018) <ftpmaster@ubuntu.com>
+
+shouvon@dgx1-DGX-Station:~$
+
+
