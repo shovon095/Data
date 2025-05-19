5067ce3c3cfca691564f76128ee8b37fac88e259
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-18 23:12:28 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,64 +1,135 @@
-drwxrwxr-x  3 shouvon shouvon       4096 Dec 19  2023 __MACOSX
-drwxr-xr-x 71 shouvon shouvon       4096 May 17 01:25 train_databases
--rw-r--r--  1 shouvon shouvon 9347158408 Dec 19  2023 train_databases.zip
--rw-rw-r--  1 shouvon shouvon    1733605 Dec 19  2023 train_gold.sql
--rw-rw-r--  1 shouvon shouvon    4321661 Dec 19  2023 train.json
--rw-rw-r--  1 shouvon shouvon     726087 Dec 19  2023 train_tables.json
-(gpt) shouvon@dgx3-DGX-Station:~/DAMO-ConvAI/bird/llm/data/train$ cd ..
-(gpt) shouvon@dgx3-DGX-Station:~/DAMO-ConvAI/bird/llm/data$ cd ..
-(gpt) shouvon@dgx3-DGX-Station:~/DAMO-ConvAI/bird/llm$ python3 llama.py     --do_train     --train_path  ./data/train/train.json     --db_root_path ./data/train/train_databases/     --engine meta-llama/Llama-2-7b-hf     --output_dir checkpoints/llama_bird_ft     --num_train_epochs 3     --per_device_train_batch_size 2     --learning_rate 3e-4
-/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/huggingface_hub/file_download.py:1150: FutureWarning: `resume_download` is deprecated and will be removed in version 1.0.0. Downloads always resume when possible. If you want to force a new download, use `force_download=True`.
-  warnings.warn(
-The `load_in_4bit` and `load_in_8bit` arguments are deprecated and will be removed in the future versions. Please, pass a `BitsAndBytesConfig` object in `quantization_config` argument instead.
-Loading checkpoint shards: 100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 2/2 [00:02<00:00,  1.37s/it]
-/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/huggingface_hub/file_download.py:1150: FutureWarning: `resume_download` is deprecated and will be removed in version 1.0.0. Downloads always resume when possible. If you want to force a new download, use `force_download=True`.
-  warnings.warn(
 Detected kernel version 4.15.0, which is below the recommended minimum of 5.5.0; this can cause the process to hang. It is recommended to upgrade the kernel to the minimum version or higher.
+  0%|                                                                                                                                                                                      | 0/441 [00:00<?, ?it/s]`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`.
+`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`.
+`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`.
+`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`.
+/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/utils/checkpoint.py:90: UserWarning: None of the inputs have requires_grad=True. Gradients will be None
+  warnings.warn(
+/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/utils/checkpoint.py:90: UserWarning: None of the inputs have requires_grad=True. Gradients will be None
+  warnings.warn(
+/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/utils/checkpoint.py:90: UserWarning: None of the inputs have requires_grad=True. Gradients will be None
+  warnings.warn(
+/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/utils/checkpoint.py:90: UserWarning: None of the inputs have requires_grad=True. Gradients will be None
+  warnings.warn(
 Traceback (most recent call last):
-  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 284, in <module>
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 171, in <module>
     main()
-  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 230, in main
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 147, in main
+    trainer.train()
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 1859, in train
+Traceback (most recent call last):
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 171, in <module>
+    return inner_training_loop(
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 2203, in _inner_training_loop
+    main()
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 147, in main
+    trainer.train()
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 1859, in train
+    tr_loss_step = self.training_step(model, inputs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 3147, in training_step
+    return inner_training_loop(
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 2203, in _inner_training_loop
+    self.accelerator.backward(loss)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/accelerate/accelerator.py", line 2242, in backward
+    tr_loss_step = self.training_step(model, inputs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 3147, in training_step
+    self.scaler.scale(loss).backward(**kwargs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/_tensor.py", line 522, in backward
+    torch.autograd.backward(
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/autograd/__init__.py", line 266, in backward
+    Variable._execution_engine.run_backward(  # Calls into the C++ engine to run the backward pass
+    RuntimeErrorself.accelerator.backward(loss):
+element 0 of tensors does not require grad and does not have a grad_fn
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/accelerate/accelerator.py", line 2242, in backward
+    self.scaler.scale(loss).backward(**kwargs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/_tensor.py", line 522, in backward
+    torch.autograd.backward(
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/autograd/__init__.py", line 266, in backward
+    Variable._execution_engine.run_backward(  # Calls into the C++ engine to run the backward pass
+RuntimeError: element 0 of tensors does not require grad and does not have a grad_fn
+Traceback (most recent call last):
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 171, in <module>
+    main()
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 147, in main
     trainer.train()
   File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 1859, in train
     return inner_training_loop(
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 1888, in _inner_training_loop
-    train_dataloader = self.get_train_dataloader()
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 874, in get_train_dataloader
-    dataloader_params["sampler"] = self._get_train_sampler()
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 844, in _get_train_sampler
-    return RandomSampler(self.train_dataset)
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/utils/data/sampler.py", line 143, in __init__
-    raise ValueError(f"num_samples should be a positive integer value, but got num_samples={self.num_samples}")
-ValueError: num_samples should be a positive integer value, but got num_samples=0
-
-./
-address/
-airline/
-app_store/
-authors/
-beer_factory/
-bike_share_1/
-book_publishing_company/
-books/
-car_retails/
-cars/
-chicago_crime/
-citeseer/
-codebase_comments/
-coinmarketcap/
-college_completion/
-computer_student/
-cookbook/
-craftbeer/
-cs_semester/
-disney/
-donor/
-european_football_1/
-food_inspection/
-food_inspection_2/
-genes/
-hockey/
-human_resources/
-ice_hockey_draft/
-image_and_language/
-language_corpus/
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 2203, in _inner_training_loop
+    tr_loss_step = self.training_step(model, inputs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 3147, in training_step
+    self.accelerator.backward(loss)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/accelerate/accelerator.py", line 2242, in backward
+    self.scaler.scale(loss).backward(**kwargs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/_tensor.py", line 522, in backward
+    torch.autograd.backward(
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/autograd/__init__.py", line 266, in backward
+    Variable._execution_engine.run_backward(  # Calls into the C++ engine to run the backward pass
+RuntimeError: element 0 of tensors does not require grad and does not have a grad_fn
+Traceback (most recent call last):
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 171, in <module>
+    main()
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama.py", line 147, in main
+    trainer.train()
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 1859, in train
+    return inner_training_loop(
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 2203, in _inner_training_loop
+    tr_loss_step = self.training_step(model, inputs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/trainer.py", line 3147, in training_step
+    self.accelerator.backward(loss)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/accelerate/accelerator.py", line 2242, in backward
+    self.scaler.scale(loss).backward(**kwargs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/_tensor.py", line 522, in backward
+    torch.autograd.backward(
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/autograd/__init__.py", line 266, in backward
+    Variable._execution_engine.run_backward(  # Calls into the C++ engine to run the backward pass
+RuntimeError: element 0 of tensors does not require grad and does not have a grad_fn
+  0%|                                                                                                                                                                                      | 0/441 [00:01<?, ?it/s]
+[2025-05-18 23:10:36,249] torch.distributed.elastic.multiprocessing.api: [ERROR] failed (exitcode: 1) local_rank: 0 (pid: 15119) of binary: /home/shouvon/anaconda3/envs/gpt/bin/python
+Traceback (most recent call last):
+  File "/home/shouvon/anaconda3/envs/gpt/bin/torchrun", line 8, in <module>
+    sys.exit(main())
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/distributed/elastic/multiprocessing/errors/__init__.py", line 347, in wrapper
+    return f(*args, **kwargs)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/distributed/run.py", line 812, in main
+    run(args)
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/distributed/run.py", line 803, in run
+    elastic_launch(
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/distributed/launcher/api.py", line 135, in __call__
+    return launch_agent(self._config, self._entrypoint, list(args))
+  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/distributed/launcher/api.py", line 268, in launch_agent
+    raise ChildFailedError(
+torch.distributed.elastic.multiprocessing.errors.ChildFailedError:
+============================================================
+llama.py FAILED
+------------------------------------------------------------
+Failures:
+[1]:
+  time      : 2025-05-18_23:10:36
+  host      : dgx3-DGX-Station
+  rank      : 1 (local_rank: 1)
+  exitcode  : 1 (pid: 15120)
+  error_file: <N/A>
+  traceback : To enable traceback see: https://pytorch.org/docs/stable/elastic/errors.html
+[2]:
+  time      : 2025-05-18_23:10:36
+  host      : dgx3-DGX-Station
+  rank      : 2 (local_rank: 2)
+  exitcode  : 1 (pid: 15121)
+  error_file: <N/A>
+  traceback : To enable traceback see: https://pytorch.org/docs/stable/elastic/errors.html
+[3]:
+  time      : 2025-05-18_23:10:36
+  host      : dgx3-DGX-Station
+  rank      : 3 (local_rank: 3)
+  exitcode  : 1 (pid: 15122)
+  error_file: <N/A>
+  traceback : To enable traceback see: https://pytorch.org/docs/stable/elastic/errors.html
+------------------------------------------------------------
+Root Cause (first observed failure):
+[0]:
+  time      : 2025-05-18_23:10:36
+  host      : dgx3-DGX-Station
+  rank      : 0 (local_rank: 0)
+  exitcode  : 1 (pid: 15119)
+  error_file: <N/A>
+  traceback : To enable traceback see: https://pytorch.org/docs/stable/elastic/errors.html
