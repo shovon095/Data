7765f3410de46bc5516006b99e2dfc90a6fed86b
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-19 23:39:29 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,10 +1,10 @@
-n 1.0.0. Downloads always resume when possible. If you want to force a new download, use `force_download=True`.
+/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/huggingface_hub/file_download.py:1150: FutureWarning: `resume_download` is deprecated and will be removed in version 1.0.0. Downloads always resume when possible. If you want to force a new download, use `force_download=True`.
   warnings.warn(
-Loaded 10 examples.
+Loaded 1 examples.
 Processed california_schools: exec=False
 Traceback (most recent call last):
-  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 198, in <module>
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 208, in <module>
     main(args)
-  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 133, in main
-    features["table"].append(v);  labels["table"].append(int(tok_str in tbls))
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 185, in main
+    X=np.stack(features[c]); y=np.array(labels[c])
 IndexError: too many indices for tensor of dimension 2
