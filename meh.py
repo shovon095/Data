6eb062ba683f850450bac68a848584931afc39b1
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-20 00:22:01 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,14 +1,19 @@
-  /home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/utils/hub.
-  py:462 in cached_file
-
-     459 │   │   │   return resolved_file
-     460 │   │   raise EnvironmentError(f"There was a specific connection error when t
-     461 │   except HFValidationError as e:
-  ❱  462 │   │   raise EnvironmentError(
-     463 │   │   │   f"Incorrect path_or_model_id: '{path_or_repo_id}'. Please provide
-     464 │   │   ) from e
-     465 │   return resolved_file
-────────────────────────────────────────────────────────────────────────────────────────
-OSError: Incorrect path_or_model_id: 'path/to/finetuned-llama'. Please provide either
-the path to a local folder or the repo_id of a model on the Hub.
+ValueError: Expected input batch_size (113) to match target batch_size (0).
+Traceback:
+File "/home/shouvon/DAMO-ConvAI/bird/llm/inter_dashboard.py", line 73, in <module>
+    out = model(input_ids=input_ids, labels=target_id)
+File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
+    return self._call_impl(*args, **kwargs)
+File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
+    return forward_call(*args, **kwargs)
+File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/models/llama/modeling_llama.py", line 1241, in forward
+    loss = loss_fct(shift_logits, shift_labels)
+File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
+    return self._call_impl(*args, **kwargs)
+File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
+    return forward_call(*args, **kwargs)
+File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/loss.py", line 1179, in forward
+    return F.cross_entropy(input, target, weight=self.weight,
+File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/functional.py", line 3059, in cross_entropy
+    return torch._C._nn.cross_entropy_loss(input, target, weight, _Reduction.get_enum(reduction), ignore_index, label_smoothing)
 
