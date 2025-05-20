2c9d7b7ca32a2289356887ada45332d3e49e476d
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-19 22:59:01 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,22 +1,12 @@
-decorations.
+LlamaModel is using LlamaSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.
+/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py:55: UserWarning: Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all Axes decorations.
   plt.tight_layout()
-Traceback (most recent call last):
-  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 225, in <module>
-    main(args)
-  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 186, in main
-    lp_full = model(input_ids=inputs.input_ids, labels=target_ids).loss.item() * target_ids.size(1)
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
-    return self._call_impl(*args, **kwargs)
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
-    return forward_call(*args, **kwargs)
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/models/llama/modeling_llama.py", line 1241, in forward
-    loss = loss_fct(shift_logits, shift_labels)
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
-    return self._call_impl(*args, **kwargs)
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
-    return forward_call(*args, **kwargs)
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/modules/loss.py", line 1179, in forward
-    return F.cross_entropy(input, target, weight=self.weight,
-  File "/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/torch/nn/functional.py", line 3059, in cross_entropy
-    return torch._C._nn.cross_entropy_loss(input, target, weight, _Reduction.get_enum(reduction), ignore_index, label_smoothing)
-ValueError: Expected input batch_size (38) to match target batch_size (129).
+Processed 0: exec=False
+/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py:55: UserWarning: Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all Axes decorations.
+  plt.tight_layout()
+Processed 0: exec=False
+/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py:55: UserWarning: Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all Axes decorations.
+  plt.tight_layout()
+Processed 0: exec=False
+/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py:55: UserWarning: Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all Axes decorations.
+  plt.tight_layout()
