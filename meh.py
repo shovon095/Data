b0a6db95ee09b8b338a570a1baaf3569c8c1bd73
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-20 00:19:11 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,11 +1,14 @@
- streamlit run inter_dashboard.py --server.port 8505
+  /home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/transformers/utils/hub.
+  py:462 in cached_file
 
-Collecting usage statistics. To deactivate, set browser.gatherUsageStats to false.
-
-
-  You can now view your Streamlit app in your browser.
-
-  Local URL: http://localhost:8505
-  Network URL: http://10.32.25.181:8505
-  External URL: http://129.207.1.24:8505
+     459 │   │   │   return resolved_file
+     460 │   │   raise EnvironmentError(f"There was a specific connection error when t
+     461 │   except HFValidationError as e:
+  ❱  462 │   │   raise EnvironmentError(
+     463 │   │   │   f"Incorrect path_or_model_id: '{path_or_repo_id}'. Please provide
+     464 │   │   ) from e
+     465 │   return resolved_file
+────────────────────────────────────────────────────────────────────────────────────────
+OSError: Incorrect path_or_model_id: 'path/to/finetuned-llama'. Please provide either
+the path to a local folder or the repo_id of a model on the Hub.
 
