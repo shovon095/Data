fd9bbc23160afeb76eeab5b9767d74a10dbcade5
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-20 00:16:27 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,10 +1,11 @@
-/home/shouvon/anaconda3/envs/gpt/lib/python3.9/site-packages/huggingface_hub/file_download.py:1150: FutureWarning: `resume_download` is deprecated and will be removed in version 1.0.0. Downloads always resume when possible. If you want to force a new download, use `force_download=True`.
-  warnings.warn(
-Loaded 1 examples.
-Processed california_schools: exec=False
-Traceback (most recent call last):
-  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 208, in <module>
-    main(args)
-  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 185, in main
-    X=np.stack(features[c]); y=np.array(labels[c])
-IndexError: too many indices for tensor of dimension 2
+ streamlit run inter_dashboard.py --server.port 8505
+
+Collecting usage statistics. To deactivate, set browser.gatherUsageStats to false.
+
+
+  You can now view your Streamlit app in your browser.
+
+  Local URL: http://localhost:8505
+  Network URL: http://10.32.25.181:8505
+  External URL: http://129.207.1.24:8505
+
