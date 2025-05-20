a2baf53063be0093139189df8b2f204995319a6c
shovon095 <97574108+shovon095@users.noreply.github.com>
2025-05-19 23:07:27 -0500

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,12 +1,10 @@
-LlamaModel is using LlamaSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.
-/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py:55: UserWarning: Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all Axes decorations.
-  plt.tight_layout()
-Processed 0: exec=False
-/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py:55: UserWarning: Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all Axes decorations.
-  plt.tight_layout()
-Processed 0: exec=False
-/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py:55: UserWarning: Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all Axes decorations.
-  plt.tight_layout()
-Processed 0: exec=False
-/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py:55: UserWarning: Tight layout not applied. The bottom and top margins cannot be made large enough to accommodate all Axes decorations.
-  plt.tight_layout()
+n 1.0.0. Downloads always resume when possible. If you want to force a new download, use `force_download=True`.
+  warnings.warn(
+Loaded 10 examples.
+Processed california_schools: exec=False
+Traceback (most recent call last):
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 198, in <module>
+    main(args)
+  File "/home/shouvon/DAMO-ConvAI/bird/llm/llama_interpret.py", line 133, in main
+    features["table"].append(v);  labels["table"].append(int(tok_str in tbls))
+IndexError: too many indices for tensor of dimension 2
